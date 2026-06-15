e7c94e7cf13347755250e5fe9ee0d0d44067e01b
R.I <ares1345istaken@gmail.com>
2026-06-15 11:07:13 +0200

    Implement html_templates and flash usage for returning success/failure of events

--- server.py
+++ server.py
@@ -15,33 +15,14 @@ Session(app)
 
 @app.route("/")
 def main():
-    mainHtml = ""
-    resp = None
     isIdentified = flask.session.get('userAuth')
-    if not isIdentified:
-        mainHtml = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main-page</title>
-    <link rel="stylesheet" href="style/style.css">
+    if isIdentified:
+        return flask.redirect(flask.url_for("dashboard"))      
+
     
-</head>
-<body>
-    <h1>Welcome</h1>
-    <button onclick="location.href='login'" id="login">Login</button>
-    <button onclick="location.href='sign-up'" id="sign-up">Sign-Up</button>
-</body>
-</html>
-"""
 
-    else:
-        return flask.redirect(flask.url_for("dashboard"))
-
-    resp = flask.make_response(mainHtml)
-    return resp
+    
+    return flask.render_template("index.html")
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
@@ -101,9 +82,9 @@ def dashboard():
                     passShrAcc = flask.request.form.get('passwordSharedAccount')
                     if (bcrypt.check_password_hash(tmp[0][1], passShrAcc)):
                         cur.execute("UPDATE userBase SET sharedAccountID = (?) WHERE userID == (?);", (tmp[0][0],accID))
-                        flask.flash(f"Joined shared account!")
+                        flask.flash(f"Joined shared account!", 'success')
                         return flask.redirect(flask.url_for('dashboard'))
-                    flask.flash(f"Error! wrong password")
+                    flask.flash(f"Error! wrong password", 'error')
                     return flask.redirect(flask.url_for('dashboard'))
                 
                 
@@ -116,13 +97,13 @@ def dashboard():
                     cur.execute("UPDATE bankAccounts SET money=(?)+(?) WHERE accountID == (?);", (sharedMoney, addSAccM, sAccID))
                     cur.execute("UPDATE bankAccounts SET money=(?)-(?) WHERE accountID == (?);", (money, addSAccM, accID))
                     db.commit()
-                    flask.flash(f"Added {addSAccM}€ to the shared account successfully!")
+                    flask.flash(f"Added {addSAccM}€ to the shared account successfully!", 'success')
                     return flask.redirect(flask.url_for('dashboard'))
                 if withdrawSAccM:
                     cur.execute("UPDATE bankAccounts SET money=(?)-(?) WHERE accountID == (?);", (sharedMoney, withdrawSAccM, sAccID))
                     cur.execute("UPDATE bankAccounts SET money=(?)+(?) WHERE accountID == (?);", (money, withdrawSAccM, accID))
                     db.commit()
-                    flask.flash(f"Withdrawn {withdrawSAccM}€ from the shared account successfully!")
+                    flask.flash(f"Withdrawn {withdrawSAccM}€ from the shared account successfully!", 'success')
                     return flask.redirect(flask.url_for('dashboard'))
 
 
@@ -133,7 +114,7 @@ def dashboard():
             if addAccM:
                 cur.execute("UPDATE bankAccounts SET money=(?)+(?) WHERE accountID == (?);", (money, addAccM, accID))
                 db.commit()
-                flask.flash(f"Added {addAccM}€ to your personal account successfully!")
+                flask.flash(f"Added {addAccM}€ to your personal account successfully!", 'success')
                 return flask.redirect(flask.url_for('dashboard'))
             
             if sendAccM:
@@ -141,7 +122,7 @@ def dashboard():
                 cur.execute("SELECT userBase.accountID, money, firstName FROM userBase INNER JOIN bankAccounts ON userBase.accountID = bankAccounts.accountID where email == (?);", (email,))
                 res = cur.fetchall()
                 if res == []:
-                    flask.flash(f"ERROR! User doesn't exist!")
+                    flask.flash(f"Error! User doesn't exist!", 'error')
                     return flask.redirect(flask.url_for('dashboard'))
                 else:
                     money = res[0][1]
@@ -150,13 +131,13 @@ def dashboard():
                     cur.execute("UPDATE bankAccounts SET money=(?)-(?) WHERE accountID == (?);", (money, sendAccM, flask.session.get('userAuth')))
 
                     db.commit()
-                    flask.flash(f"Sent {withdrawAccM}€ to {res[0][2]} successfully!")
+                    flask.flash(f"Sent {sendAccM}€ to {res[0][2]} successfully!", 'error')
                     return flask.redirect(flask.url_for("dashboard"))
             
             if withdrawAccM:
                 cur.execute("UPDATE bankAccounts SET money=(?)-(?) WHERE accountID == (?);", (money, withdrawAccM, accID))
                 db.commit()
-                flask.flash(f"Withdrawn {withdrawAccM}€ successfully!")
+                flask.flash(f"Withdrawn {withdrawAccM}€ successfully!", 'success')
                 return flask.redirect(flask.url_for('dashboard'))
             
             return flask.render_template("dashboard.html", name = name, accInfo = accInfo, sAccInfo = sAccInfo)
@@ -172,8 +153,6 @@ def dashboard():
 
     
 
-    #resp = flask.make_response(dashboardHtml)
-    return flask.render_template("dashboard.html", returnMess = "test")
 
 
 
@@ -193,7 +172,8 @@ def login():
                 cur.execute("SELECT password, userID FROM userBase WHERE email == (?);", (email,))
                 res = cur.fetchall()
                 if (res == []):
-                    returnMess = "Error! Account does not exist"
+                    flask.flash("Error! Account doesn't exist", 'error')
+                    return flask.redirect(flask.url_for('login'))
                 else:
                     
                     hashed_password = res[0][0]
@@ -201,37 +181,21 @@ def login():
                     if (bcrypt.check_password_hash(hashed_password, password)):
                         userAuth = res[0][1]
                         flask.session["userAuth"] = str(userAuth)
-                        returnMess = "Success!"
+                        flask.flash("Successfully login!", 'success')
                         return flask.redirect(flask.url_for('dashboard'))
                     else:
-                        returnMess = "Error! Wrong password"
+                        flask.flash("Error! Wrong password", 'error')
+                        return flask.redirect(flask.url_for('login'))
 
             except sqlite3.Error as e:
-                returnMess = "Error!"
+                flask.flash("Error!" + str(e), 'error')
+                return flask.redirect(flask.url_for('login'))
 
-    loginHtml = f"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login-bank</title>
-</head>
-<body>
-    <h1>Login!</h1>
-    <form method="POST">
-        <input required type="email" name="email" placeholder="Email">
-        <input required type="password" name="pass" placeholder="Password">
-        <input type="submit" value="Login" >
-    </form>
-    <h2>{returnMess}</h2>
-    <button onclick="location.href='/'" id="main">Back</button>
 
-</body>
-</html>
-"""
-    resp = flask.make_response(loginHtml)
-    return resp
+
+    
+    
+    return flask.render_template("login.html")
 
 @app.route("/sign-up", methods=["GET", "POST"])
 def signup():
@@ -257,46 +221,21 @@ def signup():
                 res = cur.fetchall()
                 cur.execute("UPDATE userBase SET accountID = (?) WHERE email==(?);", (res[0][0], email))
                 db.commit()
-                returnMess = "Success!"
                 flask.session["userAuth"] = str(userAuth)
+                flask.flash("Successfully signed up!", 'success')
                 return flask.redirect(flask.url_for('dashboard'))
             except sqlite3.Error as e:
                 
-                returnMess = "Error! Account already exists!"
+                
+                flask.flash("Error! Account already exists!", 'error')
+                return flask.redirect(flask.url_for('signup'))
     
 
-    signUpHtml = f"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign-Up-Bank</title>
-</head>
-<body>
-    <h1>Sign Up!</h1>
-    <form method="POST">
-        <input required type="email" name="email" placeholder="Email">
-        <input required type="text" name="firstName" placeholder="First name">
-        <input required type="text" name="lastName" placeholder="Last name">
-        <input required type="password" name="pass" placeholder="Password">
-        <input type="submit" value="Sign up" >
-    </form>
-    <h2>{returnMess}</h2>
-    <button onclick="location.href='/'" id="main">Back</button>
+    
 
-</body>
-</html>
-"""
+    
+    return flask.render_template("signup.html")
 
-    resp = flask.make_response(signUpHtml)
-    return resp
-
-@app.route("/style/style.css")
-def ff():
-    resp = flask.make_response("""""")
-    resp.headers["content-type"] = "text/css"
-    return resp
 
 
 app.run(port=1234,host="127.0.0.1") 
